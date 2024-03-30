29988a8580ce8affb4d624da4b8f891a90a26000
Dan Gilleland <dagilleland@shaw.ca>
2024-03-30 15:03:07 -0600

    Re #1 - Settings will create a new course w. evaluation items

--- src/Website/Data/BLL/CourseServices.cs
+++ src/Website/Data/BLL/CourseServices.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using WebApp.Data.DAL;
+using WebApp.Data.Models;
 
 namespace WebApp.Data.BLL;
 
@@ -9,4 +11,21 @@ public class CourseServices
     {
         _context = context;
     }
+
+    public List<Course> GetAllCourses()
+    {
+        return _context.Courses.Include(x => x.EvaluationItems).ToList();
+    }
+
+    public void AddCourse(Course course)
+    {
+        _context.Courses.Add(course);
+        _context.SaveChanges();
+    }
+
+    public void DeleteCourse(int id)
+    {
+        _context.Courses.Remove(_context.Courses.Find(id)!);
+        _context.SaveChanges();
+    }   
 }
